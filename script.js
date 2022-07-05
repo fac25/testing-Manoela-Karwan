2ed4d24becd29605533d9c6d7cb2b92b1e4a0df4
Mo Danova <99407460+modanova@users.noreply.github.com>
2022-07-05 21:57:59 +0100

    Tick task moves it to completed

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const addNewTask = document.querySelector("#add-new-task");
 const inputTask = document.querySelector("#input-new-task");
 const taskTemplate = document.querySelector("#task_template");
+const finishedTaskTemplate = document.querySelector("#finished-task_template");
 const toDoList = document.querySelector("#to-do-list");
+const completedList = document.querySelector("#completed-list");
 
 addNewTask.addEventListener('click', () => {
     inputTask.style.display = "block";
@@ -30,8 +32,9 @@ inputTask.addEventListener('keypress', (e) => {
 
         //Reset input
         newTaskTitle = inputTask.querySelector("#task0").value = '';
-        const closeBtn = toDoList.querySelector(".fa-x");
+
         // Add event listener to delete on-click
+        const closeBtn = toDoList.querySelector(".fa-x");
         closeBtn.addEventListener('click', (e) => {
             e.path[2].remove();
         });
@@ -42,6 +45,39 @@ inputTask.addEventListener('keypress', (e) => {
                 e.path[2].remove();
             }
         });
+
+        // Add event listener to mark as completed
+        const tickBox = toDoList.querySelector("input");
+        tickBox.addEventListener('click', (e) => {
+            // TODO Create new to-do task element in completed tasks
+            // Get a list element from the template
+        const oldTask = finishedTaskTemplate.content.cloneNode(true);
+            // Take the input's entered value
+        const newTaskTitle = e.path[1].querySelector("p").textContent;
+        // Update the new list's p tag with the input's value
+        const p = oldTask.querySelector("p[class='title']");
+        p.textContent = newTaskTitle;
+        // Add updated list to to-do list at the top of the list
+            completedList.prepend(oldTask);
+            
+            // Add event listener to delete on-click
+        const closeBtn = completedList.querySelector(".fa-x");
+        closeBtn.addEventListener('click', (e) => {
+            e.path[2].remove();
+        });
+
+        // Add event listener to delete on-enter when focused
+        closeBtn.addEventListener('keypress', (e) => {
+            if (e.key == 'Enter') {
+                e.path[2].remove();
+            }
+        });
+
+            // TODO Update completed counter 
+
+            // Delete element
+            e.path[1].remove();
+        })
     }
 })
 
