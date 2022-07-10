5d391e60e3f496cf111801891a5fadbfd07633d4
Mo Danova <99407460+modanova@users.noreply.github.com>
2022-07-10 12:53:20 +0100

    Testing tick off tasks

--- index.test.js
+++ index.test.js
@@ -4,9 +4,9 @@ test('Test works', () => {
 })
 
 // Test setup
-const createNewTask = () => {
+const createNewTask = (title) => {
     addTaskBtn.click();
-    aTask.value = "A test task";
+    aTask.value = title;
     inputToTask({ "key": "Enter" });
 }
 
@@ -19,7 +19,7 @@ const createNewTask = () => {
 
 test("deleteTask() to remove the first task of current", () => {
     // Create a task
-    createNewTask();
+    createNewTask("Test task");
 
     // Delete first task in the list
     const toBeClickedTask =  currentTasks.querySelector("li");
@@ -39,16 +39,15 @@ test("deleteTask() to remove the first task of current", () => {
 });
 
 test("deleteTask() to remove any task from a list of 10 current tasks", () => {
-    // Create a task
+    // Create 10 tasks
     let count = 10;
     while (count > 0) {
-        createNewTask();
+        createNewTask("Test task");
         count--;
     }
 
     // Delete any task in the list
     const toBeClickedArr = currentTasks.querySelectorAll("li");
-
     const toBeClickedTask = toBeClickedArr[Math.floor(Math.random()*10)]
     const toBeClickedBtn = currentTasks.querySelector("button");
     const randomId = Math.round(Math.random()*100);
@@ -81,7 +80,7 @@ test("deleteTask() to remove any task from a list of 10 current tasks", () => {
 
 test("deleteTask() to remove the first task of current", () => {
     // Create a task and mark it as finished
-    createNewTask();
+    createNewTask("Test task");
     currentTasks.querySelector("input").click();
 
     // Delete first task in the list
@@ -106,7 +105,7 @@ test("deleteTask() to remove any task from a list of 10 current tasks", () => {
     // // Create 10 tasks and mark them as finished
     let count = 10;
     while (count > 0) {
-    createNewTask();
+    createNewTask("Test task");
     currentTasks.querySelector("input").click();
     count--;
     }
@@ -137,3 +136,179 @@ test("deleteTask() to remove any task from a list of 10 current tasks", () => {
         li.querySelector("button").click();
     }
 });
+
+
+///////////////////////////
+//       Tick            //
+//  TEST current tasks   //
+///////////////////////////
+
+test("checkTask() to send the first task ticked off in finished tasks", () => {
+    // Create a task
+        createNewTask("Test task");
+
+    
+    // Tick first task in the list
+
+    const selectedTask = currentTasks.querySelector("li");
+    let taskTxt = currentTasks.querySelector("p").textContent;
+    const tickBtn = selectedTask.querySelector("input");
+    
+    const currCountBefore = countCurrentTasks;
+    const tickedCountBefore = countFinishedTasks;
+
+    tickBtn.click();
+
+    const currCountAfter = countCurrentTasks;
+    const tickedCountAfter = countFinishedTasks;
+
+    const tickedTxt = finishedTasks.querySelector("p").textContent;
+
+
+    let result = false;
+    if (tickedTxt == taskTxt) {
+        result = true;
+    }
+    equal(result, true);
+    
+    result = false;
+    if (currCountAfter == (currCountBefore - 1) &&
+        tickedCountAfter == (tickedCountBefore + 1)) {
+        result = true;
+    }
+    equal(result, true);
+
+    // Reset tasks
+    let deleteAllTicked = finishedTasks.querySelectorAll("li");
+    for (let li of deleteAllTicked) {
+        li.querySelector("button").click();
+    }
+});
+
+test("checkTask() to send the any task ticked off in finished tasks", () => {
+    // Create 10 tasks
+    let count = 10;
+    while (count > 0) {
+        createNewTask("Test task");
+        count--;
+    }
+    
+    // Delete any task in the list
+    const toBeClickedArr = currentTasks.querySelectorAll("li");
+    
+    const randomIdx = Math.round(Math.random() * 9);
+    const selectedTask = toBeClickedArr[randomIdx];
+
+    selectedTask.querySelector("p").textContent = "This task will be ticked";
+    
+    let taskTxt = selectedTask.querySelector("p").textContent;
+
+    const tickBtn = selectedTask.querySelector("input");
+    
+    const currCountBefore = countCurrentTasks;
+    const tickedCountBefore = countFinishedTasks;
+
+    tickBtn.click();
+
+    const currCountAfter = countCurrentTasks;
+    const tickedCountAfter = countFinishedTasks;
+   
+    // // compare it to the task on top of finished (use code below)
+    const tickedTxt = finishedTasks.querySelector("p").textContent;
+    
+    let result = false;
+    if (tickedTxt == taskTxt) {
+        result = true;
+    }
+    equal(result, true);
+    
+    result = false;
+    if (currCountAfter == (currCountBefore - 1) &&
+        tickedCountAfter == (tickedCountBefore + 1)) {
+        result = true;
+    }
+    equal(result, true);
+    
+    
+    // Reset notes
+    let deleteAllCurr = currentTasks.querySelectorAll("li");
+    for (let li of deleteAllCurr) {
+        li.querySelector("button").click();
+    }
+    let deleteAllTicked = finishedTasks.querySelectorAll("li");
+    for (let li of deleteAllTicked) {
+        li.querySelector("button").click();
+    }
+});
+
+test("checkTask() to send the any task ticked off on the top of finished tasks", () => {
+    // Create 10 tasks
+    let count = 10;
+    while (count > 0) {
+        createNewTask("Test task");
+        count--;
+    }
+    // Create 10 more tasks and tick them off
+    count = 10;
+    while (count > 0) {
+        createNewTask("Ticked task");
+        currentTasks.querySelector("input").click();
+        count--;
+    }
+
+
+    // Delete any task in the list
+    const toBeClickedArr = currentTasks.querySelectorAll("li");
+    
+    const randomIdx = Math.round(Math.random() * 9);
+    const selectedTask = toBeClickedArr[randomIdx];
+
+    selectedTask.querySelector("p").textContent = "This task will be ticked";
+    
+    let taskTxt = selectedTask.querySelector("p").textContent;
+
+    const tickBtn = selectedTask.querySelector("input");
+    
+    const currCountBefore = countCurrentTasks;
+    const tickedCountBefore = countFinishedTasks;
+
+    tickBtn.click();
+
+    const currCountAfter = countCurrentTasks;
+    const tickedCountAfter = countFinishedTasks;
+   
+    // // compare it to the task on top of finished (use code below)
+    const tickedTxt = finishedTasks.querySelector("p").textContent;
+    
+    let result = false;
+    if (tickedTxt == taskTxt) {
+        result = true;
+    }
+    equal(result, true);
+    
+    result = false;
+    if (currCountAfter == (currCountBefore - 1) &&
+        tickedCountAfter == (tickedCountBefore + 1)) {
+        result = true;
+    }
+    equal(result, true);
+    
+    
+    // Reset notes
+    let deleteAllCurr = currentTasks.querySelectorAll("li");
+    for (let li of deleteAllCurr) {
+        li.querySelector("button").click();
+    }
+    let deleteAllTicked = finishedTasks.querySelectorAll("li");
+    for (let li of deleteAllTicked) {
+        li.querySelector("button").click();
+    }
+});
+
+
+///////////////////////////
+//        Untick         //
+//  TEST finished tasks  //
+///////////////////////////
+
+
